b661323a73abef3195bc47c8fdeee43ecc0029b6
Zainab-dont-disturb <zainabadaranijo111@gmail.com>
2024-10-14 17:48:01 +0100

    done
    done

---
+++ script.js
@@ -0,0 +1,8 @@
+var data={
+"name":"Zainab",
+"age":13,
+"fav-food":"jellof rice",
+}
+document.getElementById(1).innerHTML=data["name"];
+document.getElementById(2).innerHTML=data["age"];
+document.getElementById(3).innerHTML=data["fav-food"];
